be44f405529a2f7270e950c4388df27ad5021534
Akshay kumar shaw <akshay.8459361606@gmail.com>
2024-11-26 19:14:25 +0530

    Jitpack publishing configured

--- aonepermissions/build.gradle.kts
+++ aonepermissions/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("maven-publish")
 }
 
 android {
@@ -22,11 +23,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -42,4 +43,16 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release"){
+                groupId = "com.aone"
+                artifactId = "aone.permissions"
+                version = "1.0-alpha"
+            }
+        }
+    }
 }
